d00a6b7cd3b6ab7fff85b6bc7f2c7fb6f886f185
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-11-22 22:43:49 +0700

    use async await to write asynchronous code

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,33 +9,37 @@ import { loadCarts } from "../data/carts.js";
 
 // async make a function return promise
 async function loadPage() {
-    console.log('load the page');
+  console.log("load the page");
 
-    // other feature that we can use in async func is, await
-    // await is like .then in Promise, it prevent the code run before await get response from promise
-    // instead of loadProductsFetch().then(...), use await for fetch data
-    // note: we can only use await inside async func
-    await loadProductsFetch();
+  // other feature that we can use in async func is, await
+  // await is like .then in Promise, it prevent the code run before await get response from promise
+  // instead of loadProductsFetch().then(...), use await for fetch data
+  // note: we can only use await inside async func
+  await loadProductsFetch();
 
-    await new Promise((resolve, reject) => {
-      loadProducts(() => {
-        // we can save this resolve value in a variable
-        resolve("value1"); // -> to throw value to another promise, just give a value in resolve func
-      });
-    })
+  await new Promise((resolve, reject) => {
+    loadProducts(() => {
+      // we can save this resolve value in a variable
+      resolve("value1"); // -> to throw value to another promise, just give a value in resolve func
+    });
+  });
 
-    // to return a value that can be use out of this function, just return it
-    return 'async value';
+  renderOrderSummaryHTML();
+  renderPaymentSummaryHTML();
+
+  // to return a value that can be use out of this function, just return it
+  return "async value";
 }
 
-// remember that async function return a promise, so we use then to get the value and go to the next step;
-loadPage().then((value) => {
-    console.log('next step');
-    
-    // we get the value of loadPage from the parameter
-    console.log(value);
-    
-});
+// // remember that async function return a promise, so we use then to get the value and go to the next step;
+// loadPage().then((value) => {
+//   console.log("next step");
+
+//   // we get the value of loadPage from the parameter
+//   console.log(value);
+// });
+
+loadPage();
 
 // we can use All method in Promise class to run some Promise at the same time
 // Promise.all([
