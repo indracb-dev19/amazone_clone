53016349ad4bfae72e31c6f71d43ba8843d44a20
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-11-22 19:49:15 +0700

    load products data from backend

--- scripts/amazone.js
+++ scripts/amazone.js
@@ -1,91 +1,95 @@
 import { Cart } from "../data/carts-class.js";
-import { products } from "../data/products.js";
-
-let productsHTML = "";
+import { loadProducts, products } from "../data/products.js";
 
 // init cart Class
-const cart = new Cart('cart');
+const cart = new Cart("cart");
 
-// set total item first load
-setTotalItem();
+function loadProductsHTML() {
+  let productsHTML = "";
 
-products.forEach((product) => {
-  // example of polymorphism is below
+  // set total item first load
+  setTotalItem();
 
-  /*
-    instead of using ternary, we can use polymorphism.
+  products.forEach((product) => {
+    // example of polymorphism is below
 
-    instead of : ${product instanceof Clothing ? 'show size chart html' : ''} (ternary)
-    use : ${product.extraInfoHtml()} (polymorphism)
-  */
-  productsHTML += `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="${product.generateImage()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+    /*
+      instead of using ternary, we can use polymorphism.
+  
+      instead of : ${product instanceof Clothing ? 'show size chart html' : ''} (ternary)
+      use : ${product.extraInfoHtml()} (polymorphism)
+    */
+    productsHTML += `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
+  
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+  
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="${product.generateImage()}">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+  
+            <div class="product-price">
+              $${product.generatePrice()}
+            </div>
+  
+            <div class="product-quantity-container">
+              <select id="select-product-qty-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+  
+            ${product.extraInfoHtml()}
+  
+            <div class="product-spacer"></div>
+  
+            <div class="added-to-cart" id="success-cart-${product.id}">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+  
+            <button class="add-to-cart-button button-primary" data-product-id="${
+              product.id
+            }">
+              Add to Cart
+            </button>
           </div>
-
-          <div class="product-price">
-            $${product.generatePrice()}
-          </div>
-
-          <div class="product-quantity-container">
-            <select id="select-product-qty-${product.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          ${product.extraInfoHtml()}
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart" id="success-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary" data-product-id="${
-            product.id
-          }">
-            Add to Cart
-          </button>
-        </div>
-    `;
-});
-
-document.getElementById("product-list").innerHTML = productsHTML;
-
-// add to cart button
-document.querySelectorAll(".add-to-cart-button").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const { productId } = btn.dataset;
-    cart.updateCart(productId);
-    cart.successAddedToCart(productId);
-    setTotalItem();
+      `;
   });
-});
 
-function setTotalItem() {
-  document.querySelector('.cart-quantity').innerHTML = cart.totalItem;
+  document.getElementById("product-list").innerHTML = productsHTML;
+
+  // add to cart button
+  document.querySelectorAll(".add-to-cart-button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const { productId } = btn.dataset;
+      cart.updateCart(productId);
+      cart.successAddedToCart(productId);
+      setTotalItem();
+    });
+  });
+
+  function setTotalItem() {
+    document.querySelector(".cart-quantity").innerHTML = cart.totalItem;
+  }
 }
+
+loadProducts(loadProductsHTML);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,8 @@
 import { renderOrderSummaryHTML } from "./checkout/orderSummary.js";
 import { renderPaymentSummaryHTML } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 
-renderOrderSummaryHTML();
-renderPaymentSummaryHTML();
+loadProducts(() => {
+    renderOrderSummaryHTML();
+    renderPaymentSummaryHTML();
+});
